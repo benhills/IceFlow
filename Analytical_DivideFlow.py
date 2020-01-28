dcd8a09b47ca739118349830ff332d32555a0f18
Benjamin Hills <benjaminhhills@gmail.com>
2020-01-29 08:37:02 +1300

    Add notes

--- Analytical_DivideFlow.py
+++ Analytical_DivideFlow.py
@@ -6,13 +6,17 @@ Created on Wed Dec 4 2018
 @author: benhills
 """
 
-import numpy as np
-
 def flow_dansgaard_johnson(ws,Eps,Eps_c):
+    """
+    Vertical velocity from Kingslake et al., 2014
+    """
     w = ws*Eps**2. / (Eps_c*(2.-Eps_c))
-    w[Eps >= Eps_c] = ws * (2.*Eps[Eps>=Eps_c]-Eps_c)/(2.-Eps_c)        
+    w[Eps >= Eps_c] = ws * (2.*Eps[Eps>=Eps_c]-Eps_c)/(2.-Eps_c)
     return w
 
 def flow_lliboutry(ws,p,Eps):
+    """
+    Vertical velocity from Kingslake et al., 2014
+    """
     w = ws * (1. - (p+2.)/(p+1.)*Eps + 1./(p+1.)*Eps**(p+2.))
     return w
