23eb40c8590db015f10e7cddb3c27873846fc560
Benjamin Hills <benjaminhhills@gmail.com>
2020-03-10 13:15:34 -0700

    Seems like the principal strain function is working well.

--- Principal_Strain.py
+++ Principal_Strain.py
@@ -32,15 +32,19 @@ def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
 
     """
 
+    if len(xs) <= 1:
+        print('Only one vector input to the strain rate calculation.')
+        return np.nan, np.nan, np.nan, np.nan
+
     # Distance from center to each point
     xdists = (xs-xc)
     ydists = (ys-yc)
 
     # Strain rates oriented with x-y
-    eps_yy = (dys-np.nanmean(dys))/ydists
-    eps_xy = (dxs-np.nanmean(dxs))/ydists
-    eps_yx = (dys-np.nanmean(dys))/xdists
-    eps_xx = (dxs-np.nanmean(dxs))/xdists
+    eps_yy = np.nanmean((dys[ydists!=0.]-np.nanmean(dys[ydists!=0.]))/ydists[ydists!=0.])
+    eps_xy = np.nanmean((dxs[ydists!=0.]-np.nanmean(dxs[ydists!=0.]))/ydists[ydists!=0.])
+    eps_yx = np.nanmean((dys[xdists!=0.]-np.nanmean(dys[xdists!=0.]))/xdists[xdists!=0.])
+    eps_xx = np.nanmean((dxs[xdists!=0.]-np.nanmean(dxs[xdists!=0.]))/xdists[xdists!=0.])
 
     # Strain rate error oriented with x-y
     if dx_errs is not None and dy_errs is not None:
@@ -51,7 +55,9 @@ def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
 
     # Principal angle
     theta_p = np.arctan(2.*np.mean([eps_xy,eps_yx])/(eps_xx-eps_yy))/2.
-
+    if (eps_xx-eps_yy) == 0. and np.mean([eps_xy,eps_yx]) == 0.:
+        theta_p = 0.
+        
     # Principal strain rates
     M1 = np.array([[np.cos(theta_p),np.sin(theta_p)],[-np.sin(theta_p),np.cos(theta_p)]])
     M2 = np.array([[eps_xx,np.mean([eps_xy,eps_yx])],[np.mean([eps_xy,eps_yx]),eps_yy]])
@@ -61,6 +67,9 @@ def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
     eps_22 = Mresult[1,1]
 
     # Strain rate error
-    eps_err = (eps_xx_err+eps_yy_err)/2. + np.nanmean([eps_xy_err,eps_yx_err])
+    if dx_errs is not None and dy_errs is not None:
+        eps_err = (eps_xx_err+eps_yy_err)/2. + np.nanmean([eps_xy_err,eps_yx_err])
+    else:
+        eps_err = None
 
     return theta_p, eps_11, eps_22, eps_err
