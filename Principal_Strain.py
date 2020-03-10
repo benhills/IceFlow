f05fa8dd5b651089591d04bbf136fadd2d87b470
Benjamin Hills <benjaminhhills@gmail.com>
2020-03-10 15:23:02 -0700

    Changed to absolute distance
    Changed from ydist>mindist to abs(ydist)>mindist
    so that all the desired points are included in the
    calculation.

--- Principal_Strain.py
+++ Principal_Strain.py
@@ -8,7 +8,7 @@ Created on Tue March 10 2020
 
 import numpy as np
 
-def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
+def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None,mindist=0.,verbose=True):
     """
     Calculate the principal strain tensor from velocity vectors
 
@@ -33,7 +33,8 @@ def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
     """
 
     if len(xs) <= 1:
-        print('Only one vector input to the strain rate calculation.')
+        if verbose:
+            print('Only one vector input to the strain rate calculation.')
         return np.nan, np.nan, np.nan, np.nan
 
     # Distance from center to each point
@@ -41,23 +42,23 @@ def principal_strain(xc,yc,xs,ys,dxs,dys,dx_errs=None,dy_errs=None):
     ydists = (ys-yc)
 
     # Strain rates oriented with x-y
-    eps_yy = np.nanmean((dys[ydists!=0.]-np.nanmean(dys[ydists!=0.]))/ydists[ydists!=0.])
-    eps_xy = np.nanmean((dxs[ydists!=0.]-np.nanmean(dxs[ydists!=0.]))/ydists[ydists!=0.])
-    eps_yx = np.nanmean((dys[xdists!=0.]-np.nanmean(dys[xdists!=0.]))/xdists[xdists!=0.])
-    eps_xx = np.nanmean((dxs[xdists!=0.]-np.nanmean(dxs[xdists!=0.]))/xdists[xdists!=0.])
+    eps_yy = np.nanmean((dys[abs(ydists)>mindist]-np.nanmean(dys[abs(ydists)>mindist]))/ydists[abs(ydists)>mindist])
+    eps_xy = np.nanmean((dxs[abs(ydists)>mindist]-np.nanmean(dxs[abs(ydists)>mindist]))/ydists[abs(ydists)>mindist])
+    eps_yx = np.nanmean((dys[abs(xdists)>mindist]-np.nanmean(dys[abs(xdists)>mindist]))/xdists[abs(xdists)>mindist])
+    eps_xx = np.nanmean((dxs[abs(xdists)>mindist]-np.nanmean(dxs[abs(xdists)>mindist]))/xdists[abs(xdists)>mindist])
 
     # Strain rate error oriented with x-y
     if dx_errs is not None and dy_errs is not None:
-        eps_yy_err = (dy_errs)/ydists
-        eps_xy_err = (dx_errs)/ydists
-        eps_yx_err = (dy_errs)/xdists
-        eps_xx_err = (dx_errs)/xdists
+        eps_yy_err = np.nanmean(dy_errs[abs(ydists)>mindist]/ydists[abs(ydists)>mindist])
+        eps_xy_err = np.nanmean(dx_errs[abs(ydists)>mindist]/ydists[abs(ydists)>mindist])
+        eps_yx_err = np.nanmean(dy_errs[abs(xdists)>mindist]/xdists[abs(xdists)>mindist])
+        eps_xx_err = np.nanmean(dx_errs[abs(xdists)>mindist]/xdists[abs(xdists)>mindist])
 
     # Principal angle
     theta_p = np.arctan(2.*np.mean([eps_xy,eps_yx])/(eps_xx-eps_yy))/2.
     if (eps_xx-eps_yy) == 0. and np.mean([eps_xy,eps_yx]) == 0.:
         theta_p = 0.
-        
+
     # Principal strain rates
     M1 = np.array([[np.cos(theta_p),np.sin(theta_p)],[-np.sin(theta_p),np.cos(theta_p)]])
     M2 = np.array([[eps_xx,np.mean([eps_xy,eps_yx])],[np.mean([eps_xy,eps_yx]),eps_yy]])
