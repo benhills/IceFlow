e4709fd430374b0e41ef37fec955d94984af05a5
Benjamin Hills <benjaminhhills@gmail.com>
2020-04-02 10:17:54 -0600

    Add a description for vialov profile equation.

--- Analytical_DivideFlow.py
+++ Analytical_DivideFlow.py
@@ -7,7 +7,37 @@ Created on Wed Dec 4 2018
 """
 
 def vialov(x,L,A,adot,const):
+    """
+    Analytical ice sheet profile from Vialov (1958)
+    Equations here taken from Cuffey and Paterson (2010) section 8.10.1
+
+    Assumptions:
+        1) no sliding
+        2) flat bed
+        3) uniform accumulation
+        4) constant flowband width
+
+    Parameters
+    -----------
+    x: array
+        distance from ice divide
+    L: float
+        total length from ice divide to margin
+    A: float
+        rate factor for Glen's flow law
+    adot: float
+        accumulation rate
+    const: class
+        constants
+
+    Output
+    -----------
+    H: array
+        Ice thickness profile
+    """
+    # Equation 8.139
     K = 2*(const.n+2)**(1/const.n)/(const.rho*const.g)*(adot/(2.*A))**(1./const.n)
+    # Equation 8.138
     H = (K*(L**(1.+1./const.n)-x**(1.+1./const.n)))**(1/(2.+2./const.n))
     return H
 
